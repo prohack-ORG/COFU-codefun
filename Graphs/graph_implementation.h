60eb1f1dd871f04e95b7d0c8c8a6bc5640efab82
hackmonster <mrsharma.sid@gmail.com>
2016-02-07 22:40:24 +0530

    [Fixed make]
    Fixed build issues
    had to make Graph() constructor inline
    to prevent multiple definitions in main()

--- Graphs/graph_implementation.h
+++ Graphs/graph_implementation.h
@@ -7,9 +7,8 @@ Date   : 7th Feb 2016
 
 #include <iostream>
 #include <stdlib.h>
+
 using namespace std;
-
-
 struct Node{
 	int dest_node;
 	Node *next_node;
@@ -23,19 +22,15 @@ class Graph{
 	int num_nodes;
 	struct Node_list *node_list;
   public :
-	Graph(int num);
+	Graph(int num){
+		num_nodes = num;
+		node_list = (struct Node_list *)malloc(sizeof(struct Node_list )*num);
+		for(int i=0;i<num;i++){
+			node_list[i].head_node=NULL;
+		}
+	}
 	void insert_edges(int src, int dest);
 	void print_adj_graph();
 };
 
-Graph::Graph(int num){
-	num_nodes = num;
-	node_list = (struct Node_list *)malloc(sizeof(struct Node_list )*num);
-	//cout<<"init1"<<endl;
-	for(int i=0;i<num;i++){
-		//cout<<"init[i] : "<<i<<endl;
-		node_list[i].head_node=NULL;
-	}
-	//cout<<"init done"<<endl;
-}
 
