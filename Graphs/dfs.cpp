cdcf59cecb11f6361f7c4b7597038de635890d80
hackmonster <mrsharma.sid@gmail.com>
2016-02-10 21:13:48 +0530

    [Temp Commit]
    Graph DFS still giving segfault.

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -15,37 +15,33 @@ void dfs(Graph g, int start_vertex){
 	int counter = 0;
 	stack<int> s;
 	s.push(start_vertex);
-	int src = s.top();
-	s.pop();
-	while(counter!=g.get_numEdges()){
+	struct Node *ref_node;
+	while(counter!=g.get_numNodes()){
+		int src = s.top();
+		s.pop();
 		cout<<src<<"\t";
-		struct Node *ref_node = g.node_list[src].head_node;
-		//cout<<"\n**1 : "<<ref_node->dest_node;
+		ref_node = g.node_list[src].head_node;
+		cout<<"\n**1 : "<<ref_node->dest_node;
 		if(ref_node->visited){
 			cout<<"\nLoop detected in Graph at node : "<<src;
 			break;
 		}
-		
-		//cout<<"\n**2";
 		ref_node->visited = 1;
+		
+		counter++;
+		cout<<"\n**2";
 		if(ref_node==NULL && s.empty() && counter!=g.get_numEdges()){
 			cout<<"\nDisconnected Graph";
 			break;
 		}
 		
-		//cout<<"\n**3";
-		//else if(ref_node==NULL && !s.empty());
-		//else{
+		cout<<"\n**3";
 			while(ref_node!=NULL){
-				//cout<<"\nIn loop";
 				s.push(ref_node->dest_node);
+				cout<<"\nIn loop pushing "<<ref_node->dest_node<<" now";
 				ref_node = ref_node->next_node;
 			}
-		//}
 		
-		//cout<<"\n**4";
-		src = s.top();
-		s.pop();
-		counter++;
+		cout<<"\n**4";
 	}
 }
--- Graphs/main.cpp
+++ Graphs/main.cpp
@@ -10,15 +10,17 @@ Date   : 6th Feb 2016
 // Function to take input, drive the graph and give output
 int main(){
 	Graph g(5);
-	g.insert_edges(0,4);		
+	g.insert_edges(0,1);		
+	g.insert_edges(0,2);		
 	g.insert_edges(1,3);		
-	g.insert_edges(2,1);		
-	g.insert_edges(3,3);		
-	g.insert_edges(3,2);		
+	g.insert_edges(1,4);		
+	/*
+	g.insert_edges(2,2);		
 	g.insert_edges(4,3);		
 	g.insert_edges(3,0);		
 	g.insert_edges(2,0);		
 	g.insert_edges(1,4);
+	*/
 	g.print_adj_graph();
 	dfs(g,0);	
 	cout<<endl;
