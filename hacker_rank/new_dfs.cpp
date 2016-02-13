697f725009e499713e3e515855883034284a9470
hackmonster <mrsharma.sid@gmail.com>
2016-02-13 15:13:38 +0530

    [HackerRank Solution]
    Optimised the process
    fails in some test cases

--- hacker_rank/new_dfs.cpp
+++ hacker_rank/new_dfs.cpp
@@ -22,8 +22,17 @@ _________
 #include <iostream>
 #include <list>
 #include <stack>
+#include <stdlib.h>
 
 using namespace std;
+
+long comb(long val){
+	if(val==1)
+		return 0;
+	long cal = (val%2==0)?((val-1)*(val/2)):(val*(val-1)/2);
+	//cout<<"\n"<<val<<" comb : "<<cal<<endl;
+	return cal;
+}
 
 class Graph{
 	int V,E;
@@ -45,7 +54,7 @@ class Graph{
 	}
 	void insert_node(int start, int end);
 	void dfs(int start);
-	int find_combination();
+	long find_combination();
 };
 
 void Graph::insert_node(int start, int end){
@@ -59,12 +68,12 @@ void Graph::dfs(int start){
 	if(visited[start]==true)
 		return;
 	num_dis_graphs++;
-	cout<<"\n\nSub-Graph "<<num_dis_graphs<<" DFS: ";
+	//cout<<"\n\nSub-Graph "<<num_dis_graphs<<" DFS: ";
 	int count=0;
 	s.push(start);
 	while(!s.empty()){
 		int node = s.top();
-		cout<<"\t "<<node;
+		//cout<<"\t "<<node;
 		s.pop();
 		count++;
 		visited[node]=true;
@@ -75,33 +84,56 @@ void Graph::dfs(int start){
 		}
 	}
 	node_count[num_dis_graphs-1] = count;
+	//cout<<"\nNo of nodes in graph : "<<num_dis_graphs-1<<" : "<<count; 
 }
 
-int Graph::find_combination(){
-	int sum = 0;
+long Graph::find_combination(){
+	long sum = 0;
+	//cout<<endl<<"No of Subgraphs : "<<num_dis_graphs<<endl;
+	/*
+	long temp[num_dis_graphs];
+	temp[0]=node_count[0];
+	//cout<<node_count[0]<<" ";
+	for(long i=1;i<num_dis_graphs;i++){
+		//cout<<node_count[i]<<" ";
+		temp[i]=temp[i-1]+node_count[i];
+		cout<<" "<<temp[i];
+	}
 	cout<<endl;
-	for(int i=0;i<num_dis_graphs;i++)
-		cout<<node_count[i]<<" ";
-	cout<<endl;
-	for(int i=0;i<num_dis_graphs;i++)
-		for(int j=i+1;j<num_dis_graphs;j++)
-			sum+=node_count[i]*node_count[j];
-	return sum;
+	sum=temp[num_dis_graphs-1]*(num_dis_graphs-1);
+	cout<<sum<<" ";
+	for(long i=1;i<num_dis_graphs;i++){
+		sum+=(V-temp[i-1])*(num_dis_graphs-i-1);
+		cout<<" "<<sum;
+	} 
+	*/
+	//cout<<endl;
+	for(int i=0;i<num_dis_graphs;i++){
+		//cout<<" "<<node_count[i];
+		sum+=comb(node_count[i]);
+	}
+	return (comb(V)-sum);
 }
 
 int main()
 {
 	int N,I;
 	int st_pt,en_pt;
-	int mul=1;
+	
 	cin>>N>>I;
+	int *temp=(int *)malloc(sizeof(int)*N);
 	Graph g(N);
 	for(int i=0;i<I;i++){
 		cin>>st_pt>>en_pt;
+		temp[st_pt]=1;
+		temp[en_pt]=1;
 		g.insert_node(st_pt,en_pt);
 	}
 	for(int i=0;i<N;i++)
-		g.dfs(i);
-	cout<<endl<<g.find_combination()<<endl<<endl;
+		//if(temp[i]==1){
+			//cout<<"\nDFS on node : "<<i;
+			g.dfs(i);
+		//}
+	cout<<endl<<g.find_combination()<<endl;
 	return 0;
 }
