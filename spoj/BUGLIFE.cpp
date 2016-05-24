96a124c55e7f901ec105e770c4dd3d412a0287d7
Raman Jaiswal <vipram91@gmail.com>
2016-05-24 19:44:20 +0530

    [spoj] DFS BUGLIFE solved.

--- spoj/BUGLIFE.cpp
+++ spoj/BUGLIFE.cpp
@@ -2,7 +2,6 @@
 #include<stdio.h>
 #include<list>
 
-
 using namespace std;
 
 class Solver
@@ -15,44 +14,14 @@ class Solver
         Solver(int v);
         void print();
         void addEdge(int u,int v);
-        bool DFSutils()
-        {
-            bool flag=false;
-            for(int i=0;i<V;i++)
-            {
-                if(color[i]==-1 && DFS(i,-1))
-                {
-                    flag=true; break;
-                }
-            }
-            return flag;
-        }
-
-        bool DFS(int x,int prev)
-        {
-                              
-            if(color[x]!=-1 || prev==-1 || prev==1) 
-            color[x]=0,prev=0;
-            else color[x]=1,prev=1;
-            
-            list<int>::iterator itr;
-            bool flag=false;
-            for(itr=adj[x].begin(); itr!=adj[x].end();++itr)
-            {
-                if(color[*itr]==color[x]) return true; //bug found
-                if(color[*itr]==-1 && !DFS(*itr,prev))
-                {
-                    flag=false;
-                }
-            }
-            return flag;
-        }    
+        bool DFSutils();
+        bool DFS(int x,int prev);
 };
 
 int main()
 {
     int T,N,E;
-    freopen("BUGLIFE.txt","r",stdin);
+//    freopen("BUGLIFE.txt","r",stdin);
     cin>>T;
     for(int t=1;t<=T;t++)
     {
@@ -61,41 +30,81 @@ int main()
         for(int i=0;i<E;i++)
         {
             int a,b;
-           // cin>>a>>b;
-           scanf("%d%d",&a,&b);
+            scanf("%d%d",&a,&b);
             S.addEdge(a-1,b-1);
         }
-//        S.print();
+        //        S.print();
         if(S.DFSutils()) printf("Scenario #%d:\nSuspicious bugs found!\n",t);
-            else   printf("Scenario #%d:\nNo suspicious bugs found!\n",t);
-//        S.print();
+        else   printf("Scenario #%d:\nNo suspicious bugs found!\n",t);
+        //        S.print();
     }
-return 0;
+    return 0;
 }
 
-void Solver::print()
+bool Solver::DFS(int x,int prev)
+{
+//    printf("\n %s %d %d ",__func__,x+1,prev+1);
+    if(color[x]!=-1 && color[x]==color[prev]) return true;
+    if(color[x]!=-1 && color[x]!=color[prev]) return false;
+    if(color[prev]==-1 || color[prev]==0) 
+        color[x]=1;
+    else if(color[prev]==1)
+        color[x]=0;
+
+//    cout<<endl; for(int k=0;k<V;k++) cout<<color[k]<<" "; cout<<endl;
+
+    list<int>::iterator itr;
+
+    for(itr=adj[x].begin(); itr!=adj[x].end(); ++itr)
+    {
+  //      cout<<" curr= "<<*itr+1<<" prev: "<<x+1<<endl;
+        if(*itr!=prev)
         {
-            for(int i=0;i<V;i++)
+            if(DFS(*itr,x))            
             {
-                cout<<"\n["<<i+1<<"] : ";
-                list<int>::iterator itr;
-                for(itr=adj[i].begin();itr!=adj[i].end();++itr)
-                    cout<<" ->"<<*itr+1;
+//                cout<<"\n return true at "<<x+1<<endl;
+                return true;
             }
-            cout<<endl;
-            for(int i=0;i<V;i++)
-                cout<<color[i]<<" "; cout<<endl;
         }
+    }
+//    cout<<"\n return false at "<<x+1<<endl;
+    return false;
+}
+bool Solver::DFSutils()
+{
+    bool flag=false;
+    for(int i=0;i<V;i++)
+    {
+        if(color[i]==-1 && DFS(i,i))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+void Solver::print()
+{
+    for(int i=0;i<V;i++)
+    {
+        cout<<"\n["<<i+1<<"] : ";
+        list<int>::iterator itr;
+        for(itr=adj[i].begin();itr!=adj[i].end();++itr)
+            cout<<" ->"<<*itr+1;
+    }
+    cout<<endl;
+    for(int i=0;i<V;i++)
+        cout<<color[i]<<" "; cout<<endl;
+}
 Solver::Solver(int v)
-        {
-            V=v;
-            adj=new list<int>[V];
-            color=new int[V];
-            for(int i=0;i<V;i++)
-                color[i]=-1;
-        }
+{
+    V=v;
+    adj=new list<int>[V];
+    color=new int[V];
+    for(int i=0;i<V;i++)
+        color[i]=-1;
+}
 void Solver::addEdge(int u,int v)
-        {
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
