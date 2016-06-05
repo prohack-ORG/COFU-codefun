c8296454f3e69c52f40ecb6ead0d005ad57104b1
hackmonster <mrsharma.sid@gmail.com>
2016-06-05 21:04:54 +0530

    [paliindromeSequence]
    status : debugging
    input  : input_palindromeSequence.txt
    
    Signed-off-by: hackmonster <mrsharma.sid@gmail.com>

--- LearningDP/palindromeSequence.cpp
+++ LearningDP/palindromeSequence.cpp
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define DEBUG 0 
 using namespace std;
 
 int T, N;
@@ -24,32 +25,53 @@ int check_Palindrome(int start, int end)
 			return 0;
 	return 1;
 }
+
+void print(int start, int end)
+{
+	cout<<endl;
+	for(int i=start;i<=end;i++)
+		cout<<data[i]<<" ";
+}
+
 int tempVal;
 void calculate_C(int start, int end, int C)
 {
-	if(counter >=minC )
-		return ;
-	cout<<"\n"<<start<<" "<<end;
+	//cout<<"\n"<<start<<" "<<end;
+#if DEBUG
+	print(start, end);
+#endif
+	//if(counter >=minC )
+	//	return ;
 	if(start>end)
 		return;
 	int temp = check_Palindrome(start,end);
 
 	if(temp)
 	{
+#if DEBUG
 		cout<<"\t palindrome";
+#endif
 		counter++;
+#if DEBUG
 		cout<<"\t palindrome count :"<<counter;
+#endif
+		C++;	
 		if(end == N-1){
+#if DEBUG
+			cout<<"\n___________________________________________\n";
+#endif
 			minC = minC > counter ? counter : minC;
-			counter--;
+			counter = C;
 		}
 		return;
 	}
+#if DEBUG
 	cout<<"\tNot palindrome";
+#endif
 	for(int i = start; i < end; i++)
 	{
-		calculate_C(start,i,C+1);
-		calculate_C(i+1,end,C+1);	
+		calculate_C(start,i,C);
+		calculate_C(i+1,end,C);	
 	}
 }
 
@@ -71,9 +93,10 @@ int main()
 			calculate_C(0,N-1,C);
 			//calculate_C(i+1,N-1,C);	
 			//minC = minC > counter ? counter : minC;
-			//counter = 0;
+			counter = 0;
+			C=0;
 		}
-		cout<<"# "<<test<<" "<<minC<<endl;
+		cout<<"\n# "<<test<<" "<<minC<<endl;
 	}
 	return 0;
 }
