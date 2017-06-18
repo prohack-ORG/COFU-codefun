3da61d972c6ef554587eab468a679f01727ce258
Raman Jaiswal <vipram91@gmail.com>
2017-06-18 12:11:22 +0530

    Runing Median -> Reducing Code Size

--- codingcontest/hkr/runningMedian.cpp
+++ codingcontest/hkr/runningMedian.cpp
@@ -14,62 +14,25 @@ void swap(float &a, float &b){   float t = b; b = a; a = t; }
 vector<float> minHeap, maxHeap;
 int len1, len2;
 
-void insertMinHeap(float val);
-void insertMaxHeap(float val);
-void maxHeapify(int idx);
-void minHeapify(int idx);
-float runningMedian(float val);
-
-int main()
-{
-    int n; float temp;
-    cin>>n;
-    len1 = 0; len2 = 0;
-    for(int i=0; i<n; i++)
-    {
-        cin>>temp;
-        cout<<runningMedian(temp)<<endl;
-    }
-    minHeap.resize(0);
-    maxHeap.resize(0);
-    return 0;
-}
-
 void insertMinHeap(float val){
     minHeap.push_back(val);
     len2 = minHeap.size();
-    int idx = len2/2 -1;
-    while(idx>=0){
-        int small = idx, l = 2*idx+1, r = 2*idx+2;
-        if(l < len2 && minHeap[small] > minHeap[l] )
-            small = l;
-        if(r < len2 && minHeap[small] > minHeap[r] )
-            small = r;
-        if(small != idx ){
-            swap(minHeap[small], minHeap[idx]);
-            idx = idx%2 ? idx/2 : idx/2-1;
-        }else {
-            break;
-        }    
-    }
+    int p = len2/2 -1, curr = len2-1;
+    while(p>=0 && minHeap[curr] < minHeap[p]){
+            swap(minHeap[curr], minHeap[p]);
+            curr = p;
+            p = p%2 ? p/2 : p/2-1;
+        }
 }
 void insertMaxHeap(float val){
     maxHeap.push_back(val);
     len1 = maxHeap.size();
-    int idx = len1/2 -1;;
-    while(idx>=0){
-        int large = idx, l = 2*idx+1, r = 2*idx+2;
-        if(l < len1 && maxHeap[large] < maxHeap[l] )
-            large = l;
-        if(r < len1 &&  maxHeap[large] < maxHeap[r] )
-            large = r;
-        if(large != idx){
-            swap(maxHeap[large], maxHeap[idx]);
-            idx = idx%2 ? idx/2 : idx/2-1;
-        }else {
-            break;
-        }    
-    }
+    int p = len1/2 -1, curr = len1-1;
+    while(p>=0 && maxHeap[curr] > maxHeap[p]){
+            swap(maxHeap[curr], maxHeap[p]);
+            curr = p;
+            p = p%2 ? p/2 : p/2-1;
+        }
 }
 void maxHeapify(int idx){
     int large = idx, l = 2*idx+1, r = 2*idx+2;
@@ -116,5 +79,17 @@ float runningMedian(float val){
     }
     return (len1+len2)%2 ? ((len1>len2) ? maxHeap[0] : minHeap[0]) : (maxHeap[0] + minHeap[0])/2.0;
 }
-
-
+int main()
+{
+    int n; float temp;
+    cin>>n;
+    len1 = 0; len2 = 0;
+    for(int i=0; i<n; i++)
+    {
+        cin>>temp;
+        cout<<runningMedian(temp)<<endl;
+    }
+    minHeap.resize(0);
+    maxHeap.resize(0);
+    return 0;
+}
