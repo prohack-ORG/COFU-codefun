f46880bde080d7d0ddcc8ea85be16356033132a0
Siddheshwar P Sharma <siddheshwar92@gmail.com>
2018-12-02 15:04:17 +0530

    [genericHeap : passed comparison function as template parameter]
    can change the outer compare function and pass to heap
    as per requirement
    
    TODO : pass element types as template parameter

--- CPP/genericHeap.cpp
+++ CPP/genericHeap.cpp
@@ -3,7 +3,7 @@
  *  Author : Siddheshwar P Sharma
  *  Date   : 01 Dec 2018
  *  HEAP implementation with templatized sorting
- *  Status : simple implementation done, to be templatized
+ *  Status : simple implementation done, templatized comparision function, elements to be templatized
  * 
  */
 
@@ -14,17 +14,18 @@
 using namespace std;
 
 #define MAX_ELE 10
-//template <typename T>
 //T is the comparison function to be passed as template parameter
+template <int (*cmpFunc)(int, int)>
 class heap{
     vector<int > vec;
     int mSize;
+    //F cmpFunc;
     public:
-        heap():mSize(0)         {    vec.clear();       }
-        int getSize()           {    return mSize; }
-        int getParent(int pos)  {    return (pos-1)/2;      }
-        int getLeft(int pos)     {    return (pos*2+1);    }
-        int getRight(int pos)    {    return (pos*2+2);  }
+        heap():mSize(0)         {    vec.clear();   }
+        int getSize()           {    return mSize;  }
+        int getParent(int pos)  {    return (pos-1)/2;  }
+        int getLeft(int pos)     {    return (pos*2+1); }
+        int getRight(int pos)    {    return (pos*2+2); }
         void insert(int val);
         int popTop();
         void heapify(int pos);
@@ -32,41 +33,45 @@ class heap{
         void printHeap()    {   for(auto ele : vec) cout<<ele<<" ";    cout<<endl;}
 };
 
-void heap::swap(int pos1, int pos2){
+template <int (*cmpFunc)(int, int)>
+void heap<cmpFunc>::swap(int pos1, int pos2){
     int temp = vec[pos1];
     vec[pos1] = vec[pos2];
     vec[pos2] = temp;
 }
-
-void heap::heapify(int index){
+template <int (*cmpFunc)(int, int)>
+void heap<cmpFunc>::heapify(int index){
 
     int size = getSize();
     int left = getLeft(index);
     int right = getRight(index);
     int mark = index;
-    if(left < size && vec[left] < vec[index])
+    //if(left < size && vec[left] < vec[index])
+    if(left < size && cmpFunc(vec[left], vec[index]))
         mark = left;
-    if(right < size && vec[right] < vec[mark])
+    //if(right < size && vec[right] < vec[mark])
+    if(right < size && cmpFunc(vec[right],  vec[mark]))
         mark = right;
     if(mark != index){
         swap(mark, index);
         heapify(mark);
     }
 }
-
-void heap::insert(int val){
+template <int (*cmpFunc)(int, int)>
+void heap<cmpFunc>::insert(int val){
 
     vec.push_back(val);
     mSize++;
     for(int i=mSize-1; i!=0; i=getParent(i)){
         int parent = getParent(i);
-        if(vec[i]<vec[parent]){
+        //if(vec[i]<vec[parent]){
+        if(cmpFunc(vec[i], vec[parent])){
             swap(i, parent);
         }
     }
 }
-
-int heap::popTop(){
+template <int (*cmpFunc)(int, int)>
+int heap<cmpFunc>::popTop(){
 
     int temp = vec[0];
     vec[0] = vec[--mSize];
@@ -75,9 +80,13 @@ int heap::popTop(){
     return temp;
 }
 
-int main(){
+int cmp(int a, int b){
+    return a<b;
+}
 
-    heap h;
+int main(){
+    //int (*func)() = cmp;
+    heap<cmp> h;
     int arr[] = {4, 3, 5, 2, 1, 8, 7, 9, 6};
     for(auto i: arr)
         h.insert(i);
@@ -92,7 +101,6 @@ int main(){
 
 /**
  *  TODO : 
- *          remove MAX_ELE limit
  *          make elements generic and pass type through templates
  * 
  * 
