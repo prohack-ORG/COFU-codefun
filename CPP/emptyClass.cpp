73c0b4fda071df2121d9b58d86fada54d608d280
Siddheshwar P Sharma <siddheshwar92@gmail.com>
2018-12-28 07:51:49 +0530

    [sizeof an empty class]
    empty class vs empty class with a virtual function
    sizeof comparison.

--- CPP/emptyClass.cpp
+++ CPP/emptyClass.cpp
@@ -17,7 +17,27 @@ class B{
     public:
     //adding a virtual function forces c++ compiler to place a 
     //VPTR for accessing the VTABLE
-    virtual void print(){};
+    virtual void print(){
+        std::cout<<"Hi SIDD B"<<std::endl;
+    };
+};
+
+class C : public B{
+    int i;
+    public:
+    void func(){
+        std::cout<<"Hi SIDD C "<<i<<std::endl;
+    }
+};
+
+class D : public B{
+    public:
+    void print(){
+        func();
+    }
+    void func(){
+        std::cout<<"Hi SIDD D "<<std::endl;
+    }
 };
 
 int main(){
@@ -25,5 +45,10 @@ int main(){
     B b;
     std::cout<<sizeof(A)<<" "<<sizeof(a)<<std::endl;
     std::cout<<sizeof(B)<<" "<<sizeof(b)<<std::endl;
+    D c;
+    B *base = &c;
+    base->print();
+    dynamic_cast<D*>(base)->func();
+    // dynamic_cast<C*>(base)->func();  //causes segFault
     return 0;
 }
